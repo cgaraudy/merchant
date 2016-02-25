f71d57f1d831af5351b54264a3aaab742622645a
Christine Garaudy <cgaraudy@uno.edu>
2016-02-25 12:00:09 -0600

    add windows gems and try to fix paperclip path

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,8 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Tell Paperclip where to find ImageMagick
-  Paperclip.options[:command_path] = "/usr/local/bin/"
+  # Paperclip.options[:command_path] = "/c/WINDOWS/system32/"
+  Paperclip.options[:command_path] = 'C:\Program Files (x86)\GnuWin32\bin'
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
